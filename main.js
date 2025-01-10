60ade404a7b513c642ee039c7209b4c8c22957a2
Your Name <you@example.com>
2025-01-10 13:48:13 +0300

    create visit card

---
+++ main.js
@@ -0,0 +1,23 @@
+const btn1 = document.getElementById('overlayMenu1');
+
+btn1.addEventListener('click', () => {
+    document.getElementById('dialog').showModal();
+});
+
+const btn2 = document.getElementById('overlayMenu2');   
+
+btn2.addEventListener('click', () => {
+    document.getElementById('dialog').showModal();
+});
+
+const btn3 = document.getElementById('overlayMenu3');   
+
+btn3.addEventListener('click', () => {  
+    document.getElementById('dialog').showModal();
+});
+
+const btn4 = document.getElementById('overlayMenu4');   
+
+btn4.addEventListener('click', () => {
+    document.getElementById('dialog').showModal();
+});
