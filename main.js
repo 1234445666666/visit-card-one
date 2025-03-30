43622b477ba6d6ab761148aa57c0eaaf0c8f64ef
1234445666666 <p.k.270607@gmail.com>
2025-03-30 03:52:34 +0300

    fix modal

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//модальные окна
 const btn1 = document.getElementById('overlayMenu1');
 
 btn1.addEventListener('click', () => {
@@ -20,4 +21,35 @@ const btn4 = document.getElementById('overlayMenu4');
 
 btn4.addEventListener('click', () => {
     document.getElementById('dialog').showModal();
+});
+
+
+//кнопка закрытия
+const closeBtn = document.getElementById('close__One');
+closeBtn.addEventListener('click', () => {
+    document.getElementById('dialog').close();
+});
+
+const closeBtn2 = document.getElementById('close__Two');
+closeBtn2.addEventListener('click', () => {
+    document.getElementById('dialog').close();
+});
+
+const closeBtn3 = document.getElementById('close__Three');
+closeBtn3.addEventListener('click', () => {
+    document.getElementById('dialog').close();
+});
+
+const closeBtn4 = document.getElementById('close__Four');
+closeBtn4.addEventListener('click', () => {
+    document.getElementById('dialog').close();
+});
+
+
+
+//форма
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 });
